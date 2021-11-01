ff0944663a3a893391f1acf9eef0d61d8f412ec6
Clown Warudo <gleb.mihailichencko@gmail.com>
2021-11-01 23:58:32 +0300

    init: lab 2

---
+++ Isu/Entities/Lesson.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Isu.Entities;
+
+namespace Isu.Entities
+{
+    public class Lesson
+    {
+        public Lesson(string teacher, string dayOfWeek, string auditory, uint lessonNumber, Guid groupId)
+        {
+            Teacher = teacher;
+            DayOfWeek = dayOfWeek;
+            Auditory = auditory;
+            LessonNumber = lessonNumber;
+            GroupId = groupId;
+        }
+
+        public string Auditory { get; private set; }
+        public string Teacher { get; private set; }
+        public string DayOfWeek { get; private set; }
+        public uint LessonNumber { get; private set; }
+        public Guid GroupId { get; private set; }
+    }
+}
---
+++ IsuExtra/Services/OGNPservice.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Isu.Entities;
+using Isu.Services;
+using IsuExtra.Entities;
+using IsuExtra.Tools;
+
+namespace IsuExtra.Services
+{
+    public class OGNPService
+    {
+        private List<Lesson> _curriculum = new List<Lesson>();
+        private List<Group> _groups = new List<Group>();
+        private List<OGNPdata> _studentsdata = new List<OGNPdata>();
+        public Group AddOgnp(string name, uint maxNumberOfStudents)
+        {
+            if (!((((name[1] - '0') * 100) + ((name[2] - '0') * 10) + (name[3] - '0')) <= 999 && name.Length <= 4))
+                throw new IsuExtraException("Invalid OGNP group name");
+            _groups.Add(new Group(name, maxNumberOfStudents, Guid.NewGuid()));
+            return _groups.Last();
+        }
+
+        public void RegisterStudent(Student student)
+        {
+            _studentsdata.Add(new OGNPdata(student.Id));
+        }
+
+        public OGNPdata FindDataByStudentId(Guid id)
+        {
+            return _studentsdata.FirstOrDefault(data => data.StudentId == id);
+        }
+
+        public void SignStudentToOgnp(Student student, Guid id)
+        {
+            if (FindDataByStudentId(student.Id) == null)
+                RegisterStudent(student);
+            var newData = FindDataByStudentId(student.Id).SignStudentToOgnp(id);
+            _studentsdata.Remove(FindDataByStudentId(student.Id));
+            _studentsdata.Add(newData);
+        }
+
+        public void RemoveStudentFromOgnp(Student student, Guid id)
+        {
+            var newData = FindDataByStudentId(student.Id).RemoveStudentFromOgnp(id);
+            _studentsdata.Remove(FindDataByStudentId(student.Id));
+            _studentsdata.Add(newData);
+        }
+
+        public List<Group> GetStudyStreamsByCourse(char num)
+        {
+            return _groups.Where(stream => stream.NameProperty[1] == num).ToList();
+        }
+
+        public List<Guid> GetStudentsIdsInOgnpGroup(Guid id)
+        {
+            return (from data in _studentsdata where data.OgnpIds.Contains(id) select data.StudentId).ToList();
+        }
+
+        public void AddLesson(string auditory, string teacher, string day, uint number, Guid group)
+        {
+            _curriculum.Add(new Lesson(teacher, day, auditory, number, group));
+        }
+
+        public List<Lesson> GetCurriculumByOgnpGroup(Guid id)
+        {
+            return _curriculum.Where(lesson => lesson.GroupId == id).ToList();
+        }
+
+        public List<Lesson> GetCurriculumByStudentId(Guid id)
+        {
+            var templist = new List<Lesson>();
+            foreach (var ognpid in _studentsdata.Where(data => data.StudentId == id).SelectMany(data => data.OgnpIds))
+            {
+                templist.AddRange(GetCurriculumByOgnpGroup(ognpid));
+            }
+
+            return templist;
+        }
+
+        public Group GetGroupById(Guid id)
+        {
+            foreach (var group in _groups)
+            {
+                if (group.Id == id)
+                    return group;
+            }
+
+            return null;
+        }
+
+        public bool IsStudentSignedToOgnp(Student student)
+        {
+            if (FindDataByStudentId(student.Id) != null)
+            {
+                if (FindDataByStudentId(student.Id).OgnpIds.Count != 0)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
